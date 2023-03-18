9953a875e5ee38bffc45f2e78467fb8d6c97e7a1
YassmineMachour <114674038+YassmineMachour172@users.noreply.github.com>
2023-03-18 14:39:00 +0200

    home page ready

--- frontend/src/components/Home/home.jsx
+++ frontend/src/components/Home/home.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useNavigate  } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Home.css';
+import logo from '../../images/logo.png'
+import pic from '../../images/pic.jpg'
 
 
 /* Home Component */
@@ -12,13 +14,63 @@ const Home = () => {
     const handleClickHome = () => {
         navigate('/Home');
     };
+    const handleClickSignIn=()=>{
+
+    }
+    const handleClickAboutUs=()=>{
+
+    }
     return (
-        <div id='home' className='backgroundimg'>
-            <div className='container'>
-                <div className="footer">
-                    <h5>Home</h5>
+        <div className="container">
+            <div id='home' className='backgroundcol'>
+                
+                <div className="path">
+                    <h5 className='pa'>Home</h5>
+                    <hr></hr>
                 </div>
                 
+                <div className="navbar">
+                    <div className="navbar-container">
+                        <img src={logo} className="logo"/>
+                        <div className='picture-home'>
+                            <br /><br />
+                            <img src={pic} className="pic"/>
+                        </div>
+                        <br />
+                        <center>
+                            <p className='description'>
+                                This application represents the perfect combination of workout with an 
+                                instructor and staying at home while performing the training.
+                                This application has a unique feature which is recording the 
+                                performance, sending a note or questions, marking it as done
+                                and getting from the instructor a review on your performance.
+                            </p>
+                            <br/>
+                            <br/>
+                            <div className="buttons">
+                                <button className='training-plans' onClick={handleClickHome}>Training Plans</button>
+                                <button className='application-manual' onClick={handleClickHome}>Application Manual</button>
+                            </div>
+                            <br/><br/>
+                        </center><br/> <br/> 
+                        <br/> 
+                        <br/> 
+                        <br/> 
+                        <br/> 
+                        <br/>
+                        <hr/>
+                        
+                        <center>
+                            <div className="down-buttons">
+                                <button className='home' disabled>Home</button>
+                                <button className='sign-in' onClick={handleClickSignIn}>Sign In</button>
+                                <button className='about-us' onClick={handleClickAboutUs}>About Us</button>
+                            </div>
+                        </center>
+                        
+                         
+                    </div>
+                </div>
             </div>
         </div>
     );
