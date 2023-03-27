50fb773b6e3f42b046e8af0ceb38b55cc8144d8a
YassmineMachour <114674038+YassmineMachour172@users.noreply.github.com>
2023-03-27 22:41:36 +0300

    Sign In Done

--- frontend/src/components/SignIn/SignIn.jsx
+++ frontend/src/components/SignIn/SignIn.jsx
@@ -6,73 +6,80 @@ import logo from '../../images/logo.png'
 import SignInpic from '../../images/SignInP.png'
 import  { useState } from 'react';
 
-
 const SignIn = () => {
     const navigate = useNavigate();
     return (
         <div className="container-fluid">
-            <div id='SignIn' className='backgroundcol'>
+           
+                <div className="col">
 
-                <header>
-                    <h5 className='pa'>Home --{'>'} Sign In</h5>
-                    <hr></hr>
-                </header>
-                <img src={logo} className="logo"/>
-                <div className='picture-SignIn'>
-                    <br /><br />
-                    <img src={SignInpic} className="picture-SignIn"/>
-                </div>
-
-                <br /><br />
-
-                <center>
-                    <div className="form">
-                        <div className="row"><center>
-                            <br/>
-                            <div className="circle"><center>
-                                <h1 className='questionMark'>?</h1></center>
-                            </div></center>
+                    <div className="row">
+                        <div className="path-con">
+                            <h5 className='pa'>Home --{'>'} Sign In</h5>
+                            <hr></hr>
                         </div>
-                        <br/><br/><br/>
-                        <form>
-                            <div className="input-container">
-
-                                <div className="row">
-                                    <center><input type="email" className='inputs' name="uname" required placeholder='Enter your email'/>**</center>
-                                </div>
-
-                                <br/><br/><br/>
-
-                                <div className="row">
-                                    <center><input type="password" className='inputs' name="pass" required placeholder='Enter a correct password' />**</center>
-                                </div>
-
-                                <br/>
-
-                                <div className="row">
-                                    <center><input type="submit" className='doneBtn' value='Done'/> </center>
-                                </div>
-
-                            </div> <br/>
-                        <br/> 
-                        <br/><br/><br/><br/>
-                        </form>
-                        
                     </div>
-                </center>
 
-                <hr/>
-                <footer>
+                    <div className="row">
+                        <img src={logo} className="logo"/>
+                    </div>
+
+                    <div className="row-lg-2">
+                    <br /><br />
+                        <img src={SignInpic} className="picture-SignIn"/>
+                    </div>
+                        <div className="row-lg-7">
+                            
+                                <br/>
+                                <br/>
+                                <center>
+                                <div className='cont' >
+                                    <div className="row">
+                                        <center>
+                                            <div className="circle">
+                                                <center>
+                                                    <h1 className='questionMark'>?</h1>
+                                                </center>
+                                            </div>
+                                        </center>
+                                        <br/>
+                                    </div>
+                                        <div className="row" id='form-con'>
+                                        <form>
+                                            <div className="text-con">
+                                                <center><input type="email" className='inputs' name="uname" required placeholder='Enter your email'/>**</center>
+                                                <center><input type="password" className='inputs' name="pass" required placeholder='Enter a correct password' />**</center>
+                                            </div>
+                                            <div className="forgot">
+                                                <b><a className="forA" href='#'>Forgot your password?</a></b>
+                                            </div>
+                                            <div className="submit-btn">
+                                                <center><input type="submit" className='doneBtn' value='Done'/> </center>
+                                            </div>
+                                            </form>
+                                            <div className="row" id='new-account'>
+                                            <b> <span>Don't Have An Account?</span><br/><a href='#'>Click Here!</a></b>
+                                            </div>
+                                        </div>
+                                        
+                            </div>
+                            </center>
+                        </div> 
+                    
+                    <div className="row" >
+                    
                     <center>
-                        <div className="down-buttons">
-                            <button className='home' onClick={() => navigate('../..//')}>Home</button>
-                            <button className='sign-in' disabled >Sign In</button>
-                            <button className='about-us' onClick={() => navigate('../../about')}>About Us</button>
-                        </div>
-                    </center>
-                </footer>
-            </div>
-        </div>
+                            <div className="down-buttons">
+                                <hr/>
+                                <button className='home' onClick={() => navigate('../..//')}>Home</button>
+                                <button className='sign-in' disabled >Sign In</button>
+                                <button className='about-us' onClick={() => navigate('../../about')}>About Us</button>
+                            </div>
+                        </center>
+                    </div>
+
+                </div>
+        </div>/*container-fluid*/
     );
 };
 
