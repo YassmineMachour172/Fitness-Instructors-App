77a92b2c7fdc1828bbe8f8126b7a27d52af890ac
YassmineMachour <114674038+YassmineMachour172@users.noreply.github.com>
2023-04-12 03:20:15 +0300

    server

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-const mysql = require('mysql')
-const express = require('express')
+const express = require('express');
 const cors = require('cors');
-const { USERS_TABLE, CARS_TABLE } = require('./db')
+const  Mongoose  = require('mongodb');
+const { default: mongoose, connect } = require('mongoose');
 
 const app = express() // Create express app
 const port =  process.env.PORT || 8000 // Port to listen on
@@ -9,56 +9,25 @@ const port =  process.env.PORT || 8000 // Port to listen on
 app.use(express.json());
 app.use(cors()); 
 
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "&Yassmine172",
-    database: "workoutin"
-  });
-  
-  con.connect(function(err) {
-    if (err) throw err; 
-    console.log("Connected!");
-  });
-/*// Define object with db config data
-const db_config = {
-    user: "root",
-    host: "localhost",
-    password: "&Yassmine172",
-    database: "workoutin",
-    port:"3306",
-};
+//Database
+const database =module.exports=() =>{
+  const connectionParams = {
+    useNewUrlParser :true,
+    useUnifiedTopology: true,
+  }
+  try{
+    mongoose.connect('mongodb+srv://yassminemach:Ya123456@cluster0.dxdqjyq.mongodb.net/mydb?retryWrites=true&w=majority',connectionParams)
+    console.log('success')
+  }catch(error){
+    console.log(error)
+    console.log('not successful')
+  }
+ 
 
-let databaseConnection
-
-// Create connection to mySql
-function handleDisconnect() {
-    databaseConnection = mysql.createConnection(db_config) 
-
-    // The server is either down or restarting (takes a while sometimes).
-    databaseConnection.connect((err) => {
-        if (err) {
-            console.log('error when connecting to db:', err);
-            // We introduce a delay before attempting to reconnect, to avoid a hot loop, and to allow our node script to process asynchronous requests in the meantime.
-            // If you're also serving http, display a 503 error.
-            setTimeout(handleDisconnect, 2000);
-        }
-    });
-
-    databaseConnection.on('error', (err) => { // sign a function to error event
-        if (err.code === 'PROTOCOL_CONNECTION_LOST') { 
-            // Connection to the MySQL server is usually lost due to either server restart, 
-            // or a connnection idle timeout (the wait_timeout server variable configures this)
-            handleDisconnect();
-        } else {
-            throw err;
-        }
-    });
 }
 
-handleDisconnect();
-
+database();
 /* listen to port */
-/*app.listen(port, () => {
-    console.log(`Car-Service server listening on http://localhost:${port}`)
-})*/
+app.listen(port, () => {
+  console.log(`Fitness-Instructor-App server listening on http://localhost:${port}`)
+})
