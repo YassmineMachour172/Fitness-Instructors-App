145f1976efdac154b5a5983877f16d3ad78e2d08
YassmineMachour <114674038+YassmineMachour172@users.noreply.github.com>
2023-06-17 12:11:27 +0300

    server is working

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser"); //parse request parameters
 const app = express(); // Create express app
 const port = process.env.PORT || 8000; // Port to listen on
-const youtube = require('youtube-api');
-const uuid = require('uuid/v4');
-const open=require('open');
-const multer =require('multer');
-const fs = require('fs');
+
 //const uuid = uuidv4();
 const url =
     "mongodb+srv://yassminemach:Ya123456@cluster0.dxdqjyq.mongodb.net/mydb?retryWrites=true&w=majority";
@@ -33,66 +29,7 @@ app.use(cors());
 
 const userTrainees = require("./routing/trainees");
 const userExercise = require("./routing/exercise");
-app.use("/api/trainees", userTrainees);const storage= multer.diskStorage({
-    destination:'/uploads',
-    filename(req,file,cb){
-        const newFileName = `${uuid()}-${file.originalname}`
 
-        cb(null,newFileName)
-    }
-})
-
-const uploadVideoFilel = multer({
-    storage:storage
-}).single("videoFile");
-
-app.post('./',uploadVideoFile,(req,res)=>{
-    if(req.file){
-        const filename=req.file.filename;
-        const{title,description}=req.body;
-        open(oAuth.generateAuthUrl({
-            access_type:'offline',
-            scope:'https://www.googleapis.com/auth/youtube.upload',
-            state:JSON.stringify({
-                filename,title,description
-            })
-        }))
-    }
-})
-
-app.get('/oauth2callback',(req,res)=>{
-    res.redirect('http://localhost:3000/#/TrainersLib')
-    const {filename,title,description}=JSON.parse(req.query.state)
-
-    oAuth.getToken(req.query.code,(err,tokens)=>{
-        if(err){
-            console.log(err);
-            return;
-        }
-
-        oAuth.setCredentials(tokens)
-
-        youtube.video.insert({
-            resource:{
-            snippet:{title,description},
-            status:{privacyStatus:'private'}
-        },
-        part:'snippet,status',
-        media:{
-            body:fs.createReadStream(filename)
-        }
-        },(err,data)=>{
-            console.log("Done");
-            res.send("Done");
-        })
-    })
-})
-const oAuth = youtube.authenticate({
-    type:'oauth',
-    client_id:credentials.web.client_id,
-    client_secret: credentials.web.client_secret,
-    redirect_url:credentials.web.redirect_uris[0]
-})
 
 
 /* listen to port */
